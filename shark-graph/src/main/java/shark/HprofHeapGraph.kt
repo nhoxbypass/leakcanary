6ca6e46b54e2f5024305655e3e5307d0787147e2
Artem Chubaryan <artem@squareup.com>
2020-06-22 19:52:44 -0500

    Moved readAllNonNullFieldsOfReferenceType to PathFinder.kt

--- shark-graph/src/main/java/shark/HprofHeapGraph.kt
+++ shark-graph/src/main/java/shark/HprofHeapGraph.kt
@@ -132,12 +132,15 @@ class HprofHeapGraph internal constructor(
     return index.objectIdIsIndexed(objectId)
   }
 
-  internal fun fieldName(
+  override fun fieldName(
     classId: Long,
     fieldRecord: FieldRecord
   ): String {
     return index.fieldName(classId, fieldRecord.nameStringId)
   }
+
+  override fun createFieldValuesReader(record: InstanceDumpRecord) =
+    FieldValuesReader(record, identifierByteSize)
 
   internal fun staticFieldName(
     classId: Long,
@@ -145,9 +148,6 @@ class HprofHeapGraph internal constructor(
   ): String {
     return index.fieldName(classId, fieldRecord.nameStringId)
   }
-
-  internal fun createFieldValuesReader(record: InstanceDumpRecord) =
-    FieldValuesReader(record, identifierByteSize)
 
   internal fun className(classId: Long): String {
     return index.className(classId)
